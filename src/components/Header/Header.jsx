66916494a6b33113697b1d20de0c5f097481f105
Anthony Shellman <atshellman@gmail.com>
2024-03-04 12:00:03 -0700

    Add initial layout with placeholders

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
 import './Header.css'
 
 const Header = () => {
-  
+  return (
+    <header className='header'>
+      <div className='header-content-container'>
+        <p>Logo</p>
+        <nav>
+          <p>Navbar placeholder</p>
+        </nav>
+        <p>Cart</p>
+      </div>
+    <p>Searchbar</p>
+  </header>
+  )
 }
 
 export default Header
