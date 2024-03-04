90ecd144f40a7806a9451ff0790d50649b2aa178
Anthony Shellman <atshellman@gmail.com>
2024-03-04 15:53:37 -0700

    Initial commit

---
+++ src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,13 @@
+import './SearchResults.css'
+
+const SearchResults = () => {
+  return (
+    <section className='search-results'>
+      <p>
+        Placeholder
+      </p>
+    </section>
+  )
+}
+
+export default SearchResults
