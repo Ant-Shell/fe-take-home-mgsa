9f71cc029eb825f9e6416c7ad72439cbfb943369
Anthony Shellman <atshellman@gmail.com>
2024-03-04 14:03:48 -0700

    Initial commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,33 @@
+import './Home.css'
+
+const Home = () => {
+  return (
+    <section className='home'>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Nisl vel pretium lectus quam id leo in vitae. Non pulvinar neque laoreet suspendisse interdum. 
+        Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Massa massa ultricies mi quis hendrerit dolor. Vel fringilla est ullamcorper eget. 
+        Diam in arcu cursus euismod. Nunc sed velit dignissim sodales. Suspendisse sed nisi lacus sed viverra tellus. 
+        Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. 
+        Tortor at auctor urna nunc id cursus metus. Egestas integer eget aliquet nibh praesent. Nisl suscipit adipiscing bibendum est ultricies integer. 
+        Ipsum dolor sit amet consectetur adipiscing.
+      <br/>
+      <br/>
+        Pharetra diam sit amet nisl suscipit adipiscing. Faucibus purus in massa tempor nec feugiat. Magnis dis parturient montes nascetur ridiculus. 
+        Nunc scelerisque viverra mauris in aliquam. Vitae suscipit tellus mauris a diam maecenas sed. Cursus euismod quis viverra nibh cras pulvinar mattis. 
+        Ac turpis egestas integer eget aliquet nibh praesent. Semper eget duis at tellus at urna condimentum mattis pellentesque. 
+        Porttitor leo a diam sollicitudin tempor id eu nisl. Viverra tellus in hac habitasse platea.
+      <br/>
+      <br/>
+        Id eu nisl nunc mi ipsum faucibus vitae. Dictumst quisque sagittis purus sit amet volutpat consequat. Enim blandit volutpat maecenas volutpat blandit aliquam. 
+        Rutrum quisque non tellus orci ac auctor. Nec dui nunc mattis enim ut tellus. Hendrerit dolor magna eget est lorem ipsum dolor sit amet. 
+        Cursus vitae congue mauris rhoncus aenean vel elit scelerisque. Adipiscing elit ut aliquam purus sit. Nisi porta lorem mollis aliquam ut porttitor. 
+        Pharetra convallis posuere morbi leo urna molestie at elementum. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis. 
+        Id nibh tortor id aliquet lectus proin nibh nisl condimentum. Turpis massa sed elementum tempus egestas sed sed risus pretium. 
+        Vitae nunc sed velit dignissim sodales ut eu sem. Dolor magna eget est lorem ipsum dolor sit amet consectetur.
+      </p>
+    </section>
+  )
+}
+
+export default Home
