f332c9c6dda2f6d9b39c158f229592b599762383
Anthony Shellman <atshellman@gmail.com>
2024-03-04 20:09:13 -0700

    Add prop types, add map WIP

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,13 +1,28 @@
+import PropTypes from 'prop-types'
 import './SearchResults.css'
 
-const SearchResults = () => {
+const SearchResults = ({ foodItemResult }) => {
+
+  const {text, hints} = foodItemResult
+
+  const resultsList = hints.map(hint => {
+    return (
+      <p key={hint.food.foodId}>{hint.food.label}</p>
+    )
+  })
+
   return (
     <section className='search-results'>
       <p>
-        Placeholder
+        Your results for <span className='search-results__text'>{text}</span>
       </p>
+      {resultsList}
     </section>
   )
 }
 
 export default SearchResults
+
+SearchResults.propTypes = {
+  foodItemResult: PropTypes.object
+}
