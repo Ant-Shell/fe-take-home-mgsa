06c7dab59b8566964394bd86af75cd1ca9a40c45
Anthony Shellman <atshellman@gmail.com>
2024-03-04 18:52:36 -0700

    Import prop types, add handleInput and searchItems functions

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,37 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types'
 import './SearchBar.css'
 
-const SearchBar = () => {
+const SearchBar = ({ getFoodItem }) => {
+  const [searchInput, setSearchInput] = useState("")
+
+  const handleInput = (e) => {
+    setSearchInput(e.target.value)
+}
+
+const searchItems = (e) => {
+  if (e.key === "Enter") {
+    getFoodItem(searchInput)
+    setSearchInput("")
+  }
+}
+
   return (
-    <input/>
+    <div className='input'>
+      <input
+        type="text"
+        placeholder="Search Products"
+        value={searchInput}
+        onChange={(e) => handleInput(e)}
+        onKeyDown={(e) => searchItems(e)}
+        maxLength={250}
+      />
+    </div>
   )
 }
 
 export default SearchBar
 
-
+SearchBar.propTypes = {
+  getFoodItem: PropTypes.func
+}
