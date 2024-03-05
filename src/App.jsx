c8efe1385e74d025380f43c1406891a5264a22b9
Anthony Shellman <atshellman@gmail.com>
2024-03-04 20:05:54 -0700

    Import SearchResults component, add routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+import SearchResults from './components/SearchResults/SearchResults'
 import fetchFood from './utilities/apiCalls'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [foodItemResult, setFoodItemResult] = useState({})
@@ -15,8 +17,13 @@ function App() {
   return (
     <main>
       <Header getFoodItem={getFoodItem} />
-      <Home />
-      {/* <SearchResults /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/:item"
+          element={<SearchResults foodItemResult={foodItemResult} />} 
+        />
+      </Routes>
       {/* <Footer /> */}
     </main>
   )
