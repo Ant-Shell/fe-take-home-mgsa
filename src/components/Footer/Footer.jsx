8143834871ee47ed56c60530075c8c84d2df9546
Anthony Shellman <atshellman@gmail.com>
2024-03-08 15:01:42 -0700

    Initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='footer-info'>
+        <p className='footer-name'>
+          Anthony Shellman
+        </p>
+        <p>
+          LinkedIn
+        </p>
+      </div>
+    </footer>
+  ) 
+}
+
+export default Footer
