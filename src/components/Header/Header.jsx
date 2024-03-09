0f0cfe915b7e2f1c91cadd39fb7035d8559832f7
Anthony Shellman <atshellman@gmail.com>
2024-03-08 17:08:29 -0700

    Import cart and logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom'
 import SearchBar from '../SearchBar/SearchBar'
 import PropTypes from 'prop-types'
+import cart from '../../assets/cart.svg'
+import logo from '../../assets/logo.svg'
 import './Header.css'
 
 const Header = ({ getFoodItem, toggleModal, removeFromPurchasedItems }) => {
@@ -20,11 +22,8 @@ const Header = ({ getFoodItem, toggleModal, removeFromPurchasedItems }) => {
   return (
     <header className='header'>
       <div className='header-content-container'>
-        <p className='header-cart-logo' onClick={(e) => returnHome(e)}>Logo</p>
-        <nav>
-          <p>Navbar placeholder</p>
-        </nav>
-        <p className='header-cart-img' onClick={(e) => modalToggle(e)}>Cart</p>
+        <img className='header-cart-logo' src={logo} alt='logo' onClick={(e) => returnHome(e)}/>
+        <img className='header-cart-img' src={cart} alt='cart' onClick={(e) => modalToggle(e)}/>
       </div>
     <SearchBar getFoodItem={getFoodItem} />
   </header>
