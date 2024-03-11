8f7207c3770e797405f0e1454ccef029224d8d33
Anthony Shellman <atshellman@gmail.com>
2024-03-11 14:13:53 -0600

    Rename from Home to HomePage

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import Hero from '../Hero/Hero'
+import Footer from '../Footer/Footer'
+import './HomePage.css'
+
+const HomePage = () => {
+  return (
+    <section className='home'>
+      <Hero />
+      <Footer />
+    </section>
+  )
+}
+
+export default HomePage
