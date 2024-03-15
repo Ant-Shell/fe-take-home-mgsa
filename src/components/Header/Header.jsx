30a74aefcc047f2c4fd842166bf233d5ff6550dc
Anthony Shellman <atshellman@gmail.com>
2024-03-14 19:18:47 -0600

    Rename image variable, pass props, add styling conditional

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import { useNavigate } from 'react-router-dom'
 import SearchBar from '../SearchBar/SearchBar'
 import PropTypes from 'prop-types'
-import cart from '../../assets/cart.svg'
+import cartImage from '../../assets/cart.svg'
 import logo from '../../assets/logo.svg'
 import './Header.css'
 
-const Header = ({ getFoodItem, toggleModal, removeFromPurchasedItems, hideModal }) => {
+const Header = ({ getFoodItem, toggleModal, removeFromPurchasedItems,
+hideModal, cart , showModal}) => {
   const navigate = useNavigate()
 
   const modalToggle = (e) => {
@@ -24,7 +25,9 @@ const Header = ({ getFoodItem, toggleModal, removeFromPurchasedItems, hideModal 
     <header className='header'>
       <div className='header-content-container'>
         <img className='header-cart-logo' src={logo} alt='logo' onClick={(e) => returnHome(e)}/>
-        <img className='header-cart-img' src={cart} alt='cart' onClick={(e) => modalToggle(e)}/>
+        <div className={`header-cart-img-container ${(cart.length && !showModal) ? "header-cart-img-container-pulse" : ""}`}>
+          <img className='header-cart-img' src={cartImage} alt='cart' onClick={(e) => modalToggle(e)}/>
+        </div>
       </div>
     <SearchBar getFoodItem={getFoodItem} />
   </header>
@@ -38,4 +41,6 @@ Header.propTypes = {
   removeFromPurchasedItems: PropTypes.func,
   toggleModal: PropTypes.func,
   hideModal: PropTypes.func, 
+  cart: PropTypes.array,
+  showModal: PropTypes.bool,
 }
